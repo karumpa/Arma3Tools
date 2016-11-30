65b5f07e4cfcdd3e3a2e3430dc799fedeac5ac33
karumpa <Superman777>
2016-11-29 23:33:30 -0500

    Added comments and other minor fixes

--- ArtyCalc/js/artycalc.js
+++ ArtyCalc/js/artycalc.js
@@ -1,23 +1,27 @@
 document.getElementById("btn").onclick=function(){calcArty()};
 
 function calcArty(){
+  //Main function
+  //Calculates distance and angle between 2 ppoints
   var disp = document.getElementById('display');
   disp.innerHTML = "";
-  var uPos = document.getElementById('upos').value;
-  var tgtPos = document.getElementById('tgtpos').value;
+  var uPos = document.getElementById('upos').value; //Position 1, assumed this is your position
+  var tgtPos = document.getElementById('tgtpos').value; //Poisition 2, assumed target
   if (uPos.length ==6) {
-    uPos = insZero(uPos);
+    uPos = insZero(uPos); //Turning 6 digit grids into 8 digit grids
   }
   if (tgtPos.length == 6) {
-    tgtPos = insZero(tgtPos);
+    tgtPos = insZero(tgtPos); //Again, for P2
   }
 
-  if (true) { //fix this later
+  if (true) { //fix this later, as in check if its a valid entry
     var x1,x2,y1,y2;
+    //parse strings and obtain x and y component
     x1 = getPosComp(uPos,"x");
     x2 = getPosComp(tgtPos,"x");
     y1 = getPosComp(uPos,"y");
     y2 = getPosComp(tgtPos,"y");
+
     var rads = getRads(x1,y1,x2,y2);
     var degs = radsToDegs(rads);
     var mrads = rads*1000;
@@ -33,6 +37,8 @@ function calcArty(){
 
 }
 function getScenario (x,y){
+  //returns a scenario number based on where the 2 points are
+  //x and y are really delta x and delta y
 /* This is what it should return
 812
 7 3
@@ -79,6 +85,7 @@ function getScenario (x,y){
   }
 }
 function getRads (x1,y1,x2,y2){
+  //returns the angle (0=North, increasing clockwise) between point 1 (x1,y1) and point 2 (x2,y2)
   var x = x2-x1;
   var y = y2-y1;
   var scen = getScenario(x,y);
@@ -118,23 +125,24 @@ function getRads (x1,y1,x2,y2){
       return Math.PI*3/2 + rad;
     default:
       return 0;
-
   }
-
-
-
 }
 function radsToDegs(rads){
+  //converts radians to degrees
   return rads * (180/Math.PI);
 }
 function getDist (x1,y1,x2,y2){
+  //Returns the distance between Point 1 (x1,y1) and Point 2(x2,y2)
   var num = Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))*10;
-  return num.toFixed(0);
+  return num;
 }
 function validPos (pos){
-return pos.length ==8;
+  //Not used yet? May get rid of
+  //Intent was to check if the coordinates were valid
+
 }
 function getPosComp(pos, ask){
+  //Return the component of the position, ask: x or y axis
   var take = 4;
   if (ask == "x"){
     return pos.substring(0,take);
@@ -145,5 +153,6 @@ function getPosComp(pos, ask){
 
 }
 function insZero(pos){
+  //inserts a 0 in a 6 digit grid
   return pos.substring(0,3) + 0 + pos.substring(3,pos.length) + 0;
 }
